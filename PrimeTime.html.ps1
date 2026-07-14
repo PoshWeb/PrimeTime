5ef50b01f8d4852a49b3fb0f3335068316010087
James Brundage <+@noreply.github.com>
2026-07-14 11:45:21 -0700

    feat: `PrimeTime` analytics and color ( Fixes #24, Fixes #25 )

--- PrimeTime.html.ps1
+++ PrimeTime.html.ps1
@@ -1,8 +1,18 @@
 param(
-[string]$AnalyticsId = $env:analyticsid
+[string]$AnalyticsId = $env:analyticsid,
+
+[string]
+$PaletteName = 'AdventureTime'
 )
+
+
+
+
+Set-Alias /_includes/Palette ./_includes/Palette.ps1
+Set-Alias /_includes/FeatherIcon ./_includes/FeatherIcon.ps1
+
 "<html>"
-    "<head>"
+    "<head>"    
     if ($AnalyticsId) {
         "<!-- Google tag (gtag.js) -->
         <script async src='https://www.googletagmanager.com/gtag/js?id=$($AnalyticsID)'></script>
@@ -13,14 +23,17 @@ param(
             gtag('config', '$($AnalyticsID)');
         </script>"
     }
-
+        "<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0' />"
+    if ($PaletteName) { 
+        "<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$PaletteName.css' id='palette' />"
+    }
     "<style>"
 
         "body {"
             @(
                 "max-width: 100vw"
-                "height: 100vh"        
-            ) -join ';'            
+                "height: 100vh"
+            ) -join ';'
         "}"
 
         "@keyframes left-to-right { from { translate: 10% 0 } to { translate: 90% 0 } }"
@@ -49,24 +62,44 @@ param(
 "</head>"
 "<body>"
     "<section>"
+        "<svg xmlns='http://www.w3.org/2000/svg' class='background' width='100%' height='100%'>"
         $animationLoop = "repeatCount='indefinite'"
-        foreach ($n in 63..7) {
+        $colors = 'blue', 'red', 'green', 'yellow', 'purple', 'cyan'
+        $min, $max = 4, 63
+        foreach ($n in $max..$min) {
             $prime = $primeTimes[$n].prime
-            $oppositeDur = $primeTimes[70 - $n]
-
+            $oppositeDur = $primeTimes[($max + $min) - $n]
             $percent = [Math]::Round(
                 $prime / $primeTimes[-1].Prime, 4
-            ) * 100
-            "<svg xmlns='http://www.w3.org/2000/svg' class='background' opacity='0.1' width='100%' height='100%'>"    
-                "<rect fill='transparent' stroke='currentColor'>"
-                    "<animate attributeName='width' dur='$prime' values='100%;$($percent)%;100%' $animationLoop />"
+            ) * 100        
+                "<rect fill='transparent' stroke='currentColor' stroke='$($colors[$n % $colors.Length])' class='ansi$(1 + ($n % 15))-stroke' stroke-width='$(1/7)%' opacity='$(1/7)'>"
+                    
+                    "<animate attributeName='width' dur='$prime' values='100%;$($percent)%;100%' $animationLoop />"                    
                     "<animate attributeName='height' dur='$oppositeDur' values='100%;$($percent)%;100%' $animationLoop />"
-                "</rect>"
-            "</svg>"
-        }    
+                "</rect>"        
+        }
+        "</svg>"
     "</section>"
 
-
+    "<style>"
+        ".top-grid { display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto; }"
+        ".top-left { align-self: left; }"
+        ".top-right { justify-self: right; align-self: right;  display: flex; flex-direction: row}"
+    "</style>"
+    "<section class='top-grid'>"
+        "<section class='top-left'>"
+            "<a href='/'><button>$(
+                /_includes/FeatherIcon -Icon home
+            )</button></a>"
+            "<a href='https://github.com/PoshWeb/PrimeTime/'><button>$(
+                /_includes/FeatherIcon -Icon github
+            )</button></a>"
+        "</section>"
+        "<section class='top-right'>"
+            /_includes/Palette -DefaultPalette $PaletteName
+        "</section>"
+    "</section>"
+    
     "<style>.masthead svg { height: 10% }</style>"
     "<section class='masthead'>"
         "$(./PrimeTime.svg.ps1 animated)"    
@@ -79,12 +112,19 @@ param(
             ConvertFrom-Markdown |
             Select-Object -ExpandProperty Html        
     "</section>"
+    "<section class='primetime-markdown'>"
+        "<h3>Demo</h3>"
+        "<p>The remainder of this page is a demo</p>"
+        "<p>These are the first 64 primes</p>"
+        "<p>Watch how rarely they overlap</p>"
+        "<p><b>This will repeat roughly every 100 trillion years</b></p>"
+    "</section>"
 "<section class='primetime-demo'>"
 foreach ($n in 1..64) {
-    $prime = $primeTimes[$n].Prime    
+    $prime = $primeTimes[$n - 1].Prime    
     "<article class='number primetime-$n'>
-        <button>
-            $($primeTimes[$n - 1].TimeSpan.TotalSeconds)s
+        <button onclick=`"navigator.clipboard.writeText('$prime')`" >            
+            $($primeTimes[$n - 1].Prime)
         </button>
     </article>"
 }
--- PrimeTime.psd1
+++ PrimeTime.psd1
@@ -114,9 +114,11 @@ PrivateData = @{
         PSIntro = @'
 ### About PrimeTime
 
-PrimeTime is a simple timing toolkit based around the power of primes
+PrimeTime is a simple animation framework
 
-PrimeTime defines prime intervals
+PrimeTime uses The Power of Primes
+
+PrimeTime defines prime intervals for animation
 
 Primes rarely overlap
 
@@ -134,19 +136,25 @@ Just link to the stylesheet
 
 ### PrimeTime Demos
 
-The PrimeTime logo uses eight primes (`7`,`9`, `11`,`13`, `17`, `19`, `23`, `29`,`31`)
+The PrimeTime logo animates eight primes:
 
-This will repeat every `6685349671` seconds, or _almost 212 years_.
+```PowerShell
+7 * 11 * 13 * 17 * 19 * 23 * 29 * 31
+```
 
-The PrimeTime page background uses 56 primes.
+The logo will repeat every `6685349671` seconds, or _almost 212 years_.
 
-This will repeat every `8.84753141993573E+116` seconds.
+The [PrimeTime page](https://primetime.poshweb.org/) background uses 56 primes.
 
-That's exponential notation.  This is a mind-boggling large number (so large it overflows the .NET `[TimeSpan]`).
+This background will repeat every `8.84753141993573E+116` seconds.
+
+That's exponential notation.
+
+This is a mind-boggling large number (so large it overflows the .NET `[TimeSpan]`).
 
 Turn that interval into years and it's _still_ mind-boggling.
 
-**This Animation Will Repeat every 100 Billion Years**
+**The Background Will Repeat every 100 Billion Years**
 '@
 
     } # End of PSData hashtable
